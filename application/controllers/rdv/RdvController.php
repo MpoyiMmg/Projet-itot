a4098531dfa72704077fa24b4e3e7758cd895baf
atthie <atthie27@gmail.com>
2019-06-01 15:46:25 +0200

    []UPDATE]correction des bugs

--- application/controllers/rdv/RdvController.php
+++ application/controllers/rdv/RdvController.php
@@ -3,37 +3,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class RdvController extends CI_Controller {
 
-	public function index()
-	{
-		//chargement de la methode prendre rdv
-		// $this->prendre_rdv();
-		$this->load->view('header');
-		$this->load->view('rdv');
-	}
+	// public function index()
+	// {
+	// 	//chargement de la methode prendre rdv
+	// 	// $this->prendre_rdv();
+	// 	$idRdv = $this->input->get("idHoraire");
+	// 	$data = array(
+	// 		'idRdv'=>$this->input->get("idHoraire"),
+	// 		'jour'=>$this->input->get("jour")
+
+	// 	);
+	// 	var_dump($data);die;
+	// 	//$this->load->view('header');
+	
+	// 	//$this->load->view('rdv',$data);
+	// }
 	public function prendre_rdv()
 	{
-		//recuperation des données dans la vue
-		$motif= $this->input->post('motif');
-		$date= $this->input->post('date');
-		$heure= $this->input->post('heure');
-		$duree= $this->input->post('duree');
-		$etat= $this->input->post('etat');
-		$commentaire= $this->input->post('commentaire');
-		$idEntreprise = 1;
-		$idClient = 1;
-
-		echo $commentaire, $date, $heure;
-		$data = array(
-						'motif'=> $motif,
-						'date'=> $date,
-						'heure'=> $heure,
-						'duree'=> $duree,
-						'etat'=> $etat,
-						'commentaire'=> $commentaire,
-						'idEntreprise' => $idEntreprise,
-						'idClient' => $idClient
-					);
-		$this->RdvModel->prendre_rdv($data);		
+		$nom_complet	= $this->input->post('nom_complet');
+		$motif			= $this->input->post('motif');
+		$jour			= $this->input->post('jour');
+		$etat			= $this->input->post('etat');
+		$telephone		= $this->input->post('telephone');
+		$email			=$this->input->post('email');
+		$idhoraire		= $this->input->post('idhoraire');
+		$reservation	= $this->input->post('reservation');
+			$jours = array(
+				"Lundi" 	=>"monday",
+				"Mardi" 	=>"tuesday",
+				"Mercredi"	=>"wednesday",
+				"Jeudi"		=>"thursday",
+				"Vendredi"	=>"friday",
+				"Samedi"	=>"saturday ",
+				"Dimanche"	=>"sunday"
+			);
+			$reservation_v=array(
+				"1 semaine avant" =>"7",
+				"2 semaines avant" =>"14"
+			);
+			$reservation=(int)$reservation_v[$reservation];
+			$jourHoraire=$jours[$jour];
+			//var_dump($jourHoraire);die;
+			$aujourdhui=new DateTime();
+			$format=$aujourdhui->format('Y-m-d');
+			//var_dump($jourHoraire);
+			$date = new DateTime($format.' +'.$reservation.'day');
+			$dateformat=$date->format('d-m-Y');
+			$datef = new DateTime($dateformat); 
+			$dateRDV=($datef->modify('next '.$jourHoraire))->format('d-m-Y');
+			//var_dump($dateRDV);die;
+			//var_dump($aujourdhui->format('d-m-Y'));die;	
+			$data = array(
+				'motif'=> $motif,
+				'idHoraire'=> $idhoraire,
+				'etat'=> $etat,
+				'nomComplet'=> $nom_complet,
+				'telephone'=> $telephone,
+				'email' => $email,
+				'date' => $dateRDV
+			);
+			$this->RdvModel->prendre_rdv($data);		
 		
 	}
 }
